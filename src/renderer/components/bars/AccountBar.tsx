e637d5bc82d43f89efe42631ead9c8804aa87061
Kirils Putimcevs <dyolfan@gmail.com>
2024-02-11 13:11:01 +0200

    Account registration

--- src/renderer/components/bars/AccountBar.tsx
+++ src/renderer/components/bars/AccountBar.tsx
@@ -16,28 +16,32 @@ const AccountBar = () => {
 
   return (
     <div className={s.account_bar_container}>
-      {authAccountState.isLoaded && (
-        <div className={s.currency_container}>USD</div>
-      )}
+      <div className={s.currency_container}>
+        {authAccountState.isLoaded && 'USD'}
+      </div>
       <div className={s.logo_container}>
         <img src={Logo} alt="logo" className={s.logo} />
       </div>
-      {authAccountState.isLoaded && (
-        <div className={s.account_info_container}>
-          <div className="col-span-2">Big account name</div>
-          <div className="col-span-3 flex justify-end">
-            <Button
-              text="Change account"
-              width="160px"
-              styleType={ButtonStyleType.INLINE}
-              onClick={() => {
-                dispatch(authorizeAccountSlice.actions.authorizeAccountClear());
-                navigate(routes.HOME);
-              }}
-            />
-          </div>
-        </div>
-      )}
+      <div className={s.account_info_container}>
+        {authAccountState.isLoaded && (
+          <>
+            <div className="col-span-2">Big account name</div>
+            <div className="col-span-3 flex justify-end">
+              <Button
+                text="Change account"
+                width="160px"
+                styleType={ButtonStyleType.INLINE}
+                onClick={() => {
+                  dispatch(
+                    authorizeAccountSlice.actions.authorizeAccountClear(),
+                  );
+                  navigate(routes.HOME);
+                }}
+              />
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
